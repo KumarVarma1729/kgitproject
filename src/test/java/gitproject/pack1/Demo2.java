fba1d8c7ab63e2a73a77eec5bc56c24ced76b551
varma <kumarvarma827@gmail.com>
2024-08-24 15:21:49 +0530

    second commit

--- src/test/java/gitproject/pack1/Demo2.java
+++ src/test/java/gitproject/pack1/Demo2.java
@@ -2,6 +2,8 @@ package gitproject.pack1;
 
 public class Demo2 {
 public void demo1() {
+	System.out.println("varma");
 	System.out.println("demo");
+	System.out.println("rajesh tarun sivaram");
 }
 }
---
+++ src/test/java/pack2/Clone6.java
@@ -0,0 +1,7 @@
+package pack2;
+
+public class Clone6 {
+public void cloneer() {
+	System.out.println("clone");
+}
+}
